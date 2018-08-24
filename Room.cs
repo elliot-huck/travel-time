79c1425a789be3833bcd21b8cdf77a223d3c67e2
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-08-24 13:32:13 -0500

    Added Room class

---
+++ Room.cs
@@ -0,0 +1,11 @@
+
+namespace TravelTime
+{
+  public class Room
+  {
+    public int Number { get; set; }
+    public int MaxOccupants { get; set; }
+    public double Price { get; set; }
+    public bool Available { get; set; }
+  }
+}
